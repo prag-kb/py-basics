0075f6b32e3ae7d0a31977dc9d6134dfe847b071
prag-kb <103320078+prag-kb@users.noreply.github.com>
2024-07-14 20:09:22 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,15 @@
+# def myFunc(e):
+#    return len(e)
+#
+# l1= ["I", "am", "a", "list"]
+#
+# l1.append("items")
+# l1.clear()
+# l2 = copy(l1)
+# x = l1.count("items")
+# l1.extend("available")
+# l1.insert(1, "available")
+# l1.reverse()
+# l1.remove("items")
+# l1.sort(key=myFunc)
+# print(l1)
